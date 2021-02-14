bc2eee37b1505700481faa72a45f396bd87bb431
voronrk <voron_76@mail.ru>
2021-02-14 23:14:02 +0300

    Render order card. Add order with save inputed data to variable (not to storage)

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use sctrict';
 
+let orderData={};
+
 const body=document.querySelector('body');
 
 body.addEventListener('dragenter', (event) => {
@@ -17,38 +19,51 @@ body.addEventListener('dragdrop', (event) => {
 
 const addOrder = (currentElement) => {
    let newOrder=currentElement.parentNode.appendChild(document.createElement('div'));
-   newOrder.classList.add('order');
+   // newOrder.classList.add('order');
    newOrder.classList.add('is-size-7');
-   newOrder.id='o11';
+   // newOrder.id='o11';
    newOrder.draggable="true";
    newOrder.innerHTML=`
-   <input class="input is-small" type="text" placeholder="Номер заказа" focus>
-   <input class="input is-small" type="text" placeholder="Заказчик">
-   <input class="input is-small" type="text" placeholder="Параметры заказа">
-   <input class="input is-small" type="text" placeholder="Бумага">
-   <input class="input is-small" type="text" placeholder="Формат">
-   <input class="input is-small" type="text" placeholder="Кол-во форм">
-   <input class="input is-small" type="text" placeholder="Кол-во листопрогонов">
-   <input class="input is-small" type="time" placeholder="Время план.">
-   <div class="buttons are-small">
-      <button class="button" id="save-button">Сохранить</button>
-      <button class="button" id="cancel-button">Отмена</button>
-   </div>
+      <input class="input is-small" type="text" id="num" placeholder="Номер заказа" focus>
+      <input class="input is-small" type="text" id="customer" placeholder="Заказчик">
+      <input class="input is-small" type="text" id="params" placeholder="Параметры заказа">
+      <input class="input is-small" type="text" id="paper" placeholder="Бумага">
+      <input class="input is-small" type="text" id="size" placeholder="Формат">
+      <input class="input is-small" type="text" id="plates" placeholder="Кол-во форм">
+      <input class="input is-small" type="text" id="clicks" placeholder="Кол-во листопрогонов">
+      <input class="input is-small" type="time" id="time" placeholder="Время план.">
+      <div class="buttons are-small">
+         <button class="button" id="save-button">Сохранить</button>
+         <button class="button" id="cancel-button">Отмена</button>
+      </div>
    `;
 };
 
-const renderOrderCardSmall = (orderCard) => {
-   // let orderCard=destinationNode.parentNode.appendChild(document.createElement('div'));
-   console.log(orderCard);
+const renderOrderCardSmall = (orderCard, orderData) => {
+   orderCard.classList.add('order');
+   orderCard.classList.add('is-size-7');
+   orderCard.draggable=true;
+   orderCard.id=`o${orderData['num']}`;
    orderCard.innerHTML=`
-   <table class="table isfullwidth is-bordered">
-      <tr><th>№</th><th>Формат</th><th>Л/п</th><th>Время</th></tr>
-      <tr><td>5567</td><td>60x90</td><td>3200</td><td>00:20</td></tr>
+   <table class="table is-fullwidth is-bordered is-narrow">
+      <tr><th>№</th><th>Формат</th><th>Форм</th><th>Л/п</th><th>Время</th><th>Бумага</th></tr>
+      <tr>
+         <td>${orderData['num']}</td>
+         <td>${orderData['size']}</td>
+         <td>${orderData['plates']}</td>
+         <td>${orderData['clicks']}</td>
+         <td>${orderData['time']}</td>
+         <td>${orderData['paper']}</td>
+      </tr>
+      <tr><td>Параметры</td><td colspan="5">${orderData['params']}</td></tr>
+      <tr><td>Заказчик</td><td colspan="5">${orderData['customer']}</td></tr>
    </table>
    `;
 };
 
 body.addEventListener('drop', (event) => {
+   event.stopPropagation();
+   event.preventDefault();
    console.log(event.target);
    let data = event.dataTransfer.getData("Text");
    if (event.target.id[0]=='c') {
@@ -59,8 +74,6 @@ body.addEventListener('drop', (event) => {
       console.log(event.target.parentNode.parentNode.parentNode.parentNode);
       event.target.parentNode.parentNode.parentNode.parentNode.parentNode.insertBefore(document.getElementById(data), event.target.parentNode.parentNode.parentNode.parentNode);
    };
-   event.stopPropagation();
-   event.preventDefault();
    return false;
 });
 
@@ -78,7 +91,12 @@ body.addEventListener('click', (event) => {
       addOrder(target);
    };
    if (target.id=='save-button') {
-      console.log(target.id);
-      renderOrderCardSmall(target.parentNode.parentNode);
+      console.log(target.parentNode.parentNode);
+      for (let i=0; i<=7; i++) {
+         orderData[target.parentNode.parentNode.children[i].id]=target.parentNode.parentNode.children[i].value;
+      };
+      
+      console.log(orderData);
+      renderOrderCardSmall(target.parentNode.parentNode, orderData);
    }
 });
